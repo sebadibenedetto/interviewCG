28eefdbbac2a5938cced40c0aae22a6a7bfb309e
GLOBAL\v735726 <s.dibenedetto@globant.com>
2018-05-17 10:39:42 -0300

    fix documentation

--- TheCorcoranGroup.WebAPI/Controllers/PresidentsController.cs
+++ TheCorcoranGroup.WebAPI/Controllers/PresidentsController.cs
@@ -14,6 +14,13 @@ namespace TheCorcoranGroup.WebAPI.Controllers
             _presidentDomain = presidentDomain;
         }
 
+        /// <summary>
+        /// Get the list of presidents, sorted and filtered
+        /// </summary>
+        /// <param name="columnSort">Column by which it is ordered</param>
+        /// <param name="sortDirection">Sort direction (ascending or descending)</param>
+        /// <param name="name">Property by which it is filtered (if it is empty, not filtered)</param>
+        /// <returns>Return a collection of objects president</returns>
         public IHttpActionResult Get(Columns columnSort, SortDirection sortDirection, string name = null)
         {
             return Ok(_presidentDomain.GetPresidents(name, columnSort, sortDirection).Select(x => new PresidentModel
